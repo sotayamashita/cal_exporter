afee6a8eac92f33925f6573d2f39ed7551891ccb
sota0805 <sota.yamashita@gmail.com>
2014-04-03 18:04:12 +0900

    Remove: cal_exporter.rb: divide files to require

--- lib/cal_exporter.rb
+++ lib/cal_exporter.rb
@@ -15,8 +15,8 @@ module CalExporter
     
     desc "calender CALENDER_ID FORMAT SAVE_LOCATION", "Export calender with specified file format"
     def calender(calendar_id, format, save_location)
-      cal = Importer.new(calendar_id)
-      calendars = cal.fetch
+      import = Importer.new(calendar_id)
+      calendars = import.fetch
       export = Exporter.new(format, save_location)
 
       # save
@@ -28,64 +28,9 @@ module CalExporter
     end
   end
 
-  # TODO 別ファイルにする
-  class Importer 
-
-    def initialize(calendar_id)
-      @calendar_id = calendar_id
-    end
-
-    def fetch
-      calender_url = "https://www.google.com/calendar/ical/#{@calendar_id}/public/basic.ics"  
-      calendars = open (calender_url) { |cal| Icalendar.parse(cal) }
-    end
-
-  end
-
-  # TODO 別ファイルにする
-  class Exporter
-
-  	def initialize(format, save_location)
-  	  @format   = format
-      @save_location = save_location
-  	end
-
-
-
-  	def to_jekyll(event)
-
-      output_list = {
-        "title"         => event.summary.gsub(/[|]|:/, '[' => '(', ']' => ')', ':' => '&#58;'),
-        "location"      => event.location.chomp,
-        "date"          => event.dtstart.strftime('%Y-%m-%d'),
-        "friendly_date" => event.dtstart.strftime('%A %d %b %Y'),  
-        "link"          => url_list(event.description)[0],
-        "layout"        => "post",
-        "categories"    => "meetups"
-      }
-
-  	end
-
-
-    def save(event)
-      # TODOファイルのパスを出す, ファイルパスをオブジェクトフィールドからとってくる
-
-      path = "#{@save_location}/meetups"
-
-      Dir.mkdir(path) unless Dir.exist?(path)
-
-      f = File.open("#{path}/#{event.dtstart.strftime('%Y-%m-%d')}-#{event.uid[0, 7]}.md", "w")
-      f.write(to_jekyll(event))
-      f.close 
-    end
-
-
-    def url_list(description)
-      URI.extract(description, %w[http https])
-    end
-
-  end
-
+  require 'cal_exporter/exporter.rb'
+  require 'cal_exporter/importer.rb'
+  
 
 end
 
