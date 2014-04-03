94dce5888bf7dc37cb9f79303f69a75f25b621dd
sota0805 <sota.yamashita@gmail.com>
2014-04-03 17:31:45 +0900

    Add: cal_exporter.rb: divide class

--- lib/cal_exporter.rb
+++ lib/cal_exporter.rb
@@ -13,69 +13,69 @@ require 'awesome_print'
 module CalExporter
   class CLI < Thor
     
-    desc "calender CALENDER_ID FORMAT, SAVE_LOCATION", "export calender with specified file format"
+    desc "calender CALENDER_ID FORMAT SAVE_LOCATION", "Export calender with specified file format"
     def calender(calendar_id, format, save_location)
-      cal = Exporter.new(calendar_id, format, save_location)
-      cal.fetch(calendar_id)
+      cal = Importer.new(calendar_id)
+      calendars = cal.fetch
+      export = Exporter.new(format, save_location)
+
+      # save
+      calendars.each do |calendar|
+        calendar.events.each do |event|  
+          export.save(event)
+        end
+      end
+    end
+  end
+
+  # TODO 別ファイルにする
+  class Importer 
+
+    def initialize(calendar_id)
+      @calendar_id = calendar_id
+    end
+
+    def fetch
+      calender_url = "https://www.google.com/calendar/ical/#{@calendar_id}/public/basic.ics"  
+      calendars = open (calender_url) { |cal| Icalendar.parse(cal) }
     end
 
   end
 
-
+  # TODO 別ファイルにする
   class Exporter
 
-  	def initialize(calendar_id, format, save_location)
-  	  @calendar_id   = calendar_id
+  	def initialize(format, save_location)
   	  @format   = format
-  	  @save_location = save_location
+      @save_location = save_location
   	end
 
-
-  	def fetch(calendar_id)
-      calender_url = "https://www.google.com/calendar/ical/#{calendar_id}/public/basic.ics"
-    
-      calendars = open (calender_url) { |cal| Icalendar.parse(cal) }
-
-      calendars.each do |calendar|
-        calendar.events.each do |event|  
-          to_jekyll(event)
-        end
-      end
-  	end
 
 
   	def to_jekyll(event)
-    
-      title = event.summary
-      new_title = title.sub('[', '(')
-      new_title = new_title.sub(']', ')')
-      new_title = new_title.sub(':', '&#58;') 
 
-      # event.summary.sub(['[', ']', ':'], ['(', ')', '&#58;'])
-
-      # List of output
       output_list = {
-        "title"         => new_title,
+        "title"         => event.summary.gsub(/[|]|:/, '[' => '(', ']' => ')', ':' => '&#58;'),
         "location"      => event.location.chomp,
-        "date"          => date_format(event.dtstart,'%Y-%m-%d'),
-        "friendly_date" => date_format(event.dtstart,'%A %d %b %Y'),
+        "date"          => event.dtstart.strftime('%Y-%m-%d'),
+        "friendly_date" => event.dtstart.strftime('%A %d %b %Y'),  
         "link"          => url_list(event.description)[0],
         "layout"        => "post",
         "categories"    => "meetups"
-      } 
+      }
 
-      save(output_list.to_yaml, event)
   	end
 
 
-    def save(content, event)
-      current_path = File.expand_path(File.dirname($0))
-      Dir.mkdir("#{current_path}/meetups") unless Dir.exist?("#{current_path}/meetups")
+    def save(event)
+      # TODOファイルのパスを出す, ファイルパスをオブジェクトフィールドからとってくる
 
+      path = "#{@save_location}/meetups"
 
-      # Create file
-      f = File.open("#{current_path}/meetups/#{date_format(event.dtstart,'%Y-%m-%d')}-#{event.uid[0, 7]}.md", "w")
-        f.write(content)
+      Dir.mkdir(path) unless Dir.exist?(path)
+
+      f = File.open("#{path}/#{event.dtstart.strftime('%Y-%m-%d')}-#{event.uid[0, 7]}.md", "w")
+      f.write(to_jekyll(event))
       f.close 
     end
 
@@ -84,14 +84,9 @@ module CalExporter
       URI.extract(description, %w[http https])
     end
 
-
-    def date_format(date, datetime_format)
-      d = DateTime.parse(date.to_s)
-      d.strftime(datetime_format)
-    end
-
-
   end
+
+
 end
 
 
