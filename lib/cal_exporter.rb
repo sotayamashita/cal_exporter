216fd9200a10d31dc57c7b201abf35cd740a14f9
sota0805 <sota.yamashita@gmail.com>
2014-04-03 14:30:37 +0900

    Delete: cal_exporter.rb: extra code comment outed

--- lib/cal_exporter.rb
+++ lib/cal_exporter.rb
@@ -5,6 +5,10 @@ require "thor"
 require "date"
 require "thor"
 require 'yaml'
+# For Debug 
+# TODO Delete it before publishing
+require 'pry'
+require 'awesome_print'
 
 module CalExporter
   class CLI < Thor
@@ -12,11 +16,10 @@ module CalExporter
     desc "calender CALENDER_ID FORMAT, SAVE_LOCATION", "export calender with specified file format"
     def calender(calendar_id, format, save_location)
       cal = Exporter.new(calendar_id, format, save_location)
-      cal.execute(calendar_id, format, save_location)
+      cal.fetch(calendar_id)
     end
 
   end
-
 
 
   class Exporter
@@ -28,35 +31,27 @@ module CalExporter
   	end
 
 
-  	def execute(calendar_id, format, save_location)
-  		event = fetch(calendar_id)
-  		content = to_jekyll(event)
-  		# save(save_location content)
-      save(content, event)
-  	end
-
-
   	def fetch(calendar_id)
       calender_url = "https://www.google.com/calendar/ical/#{calendar_id}/public/basic.ics"
     
       calendars = open (calender_url) { |cal| Icalendar.parse(cal) }
 
       calendars.each do |calendar|
-        calendar.events.each do |event| 
-          return event
+        calendar.events.each do |event|  
+          to_jekyll(event)
         end
       end
   	end
 
 
   	def to_jekyll(event)
-
-      url = url_list(event.description)
     
       title = event.summary
       new_title = title.sub('[', '(')
       new_title = new_title.sub(']', ')')
       new_title = new_title.sub(':', '&#58;') 
+
+      # event.summary.sub(['[', ']', ':'], ['(', ')', '&#58;'])
 
       # List of output
       output_list = {
@@ -64,23 +59,14 @@ module CalExporter
         "location"      => event.location.chomp,
         "date"          => date_format(event.dtstart,'%Y-%m-%d'),
         "friendly_date" => date_format(event.dtstart,'%A %d %b %Y'),
-        "link"          => url[0],
+        "link"          => url_list(event.description)[0],
         "layout"        => "post",
         "categories"    => "meetups"
       } 
 
-      content = output_list.to_yaml
-
-      return content
+      save(output_list.to_yaml, event)
   	end
 
-
-  	# def save(save_location, content)
-
-    #   current_path = File.expand_path(File.dirname($0))
-    #   Dir.mkdir("#{current_path}/meetup") unless Dir.exist?("#{current_path}/meetup")
-
-  	# end
 
     def save(content, event)
       current_path = File.expand_path(File.dirname($0))
@@ -88,7 +74,7 @@ module CalExporter
 
 
       # Create file
-      f = File.open("#{current_path}/meetups/#{event.uid[0, 7]}.md", "w")
+      f = File.open("#{current_path}/meetups/#{date_format(event.dtstart,'%Y-%m-%d')}-#{event.uid[0, 7]}.md", "w")
         f.write(content)
       f.close 
     end
@@ -101,7 +87,7 @@ module CalExporter
 
     def date_format(date, datetime_format)
       d = DateTime.parse(date.to_s)
-      return d.strftime(datetime_format)
+      d.strftime(datetime_format)
     end
 
 
