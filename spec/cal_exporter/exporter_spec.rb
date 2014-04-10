7a081b9729fe847fa602dbce7c8e3836ee8d4a42
sota0805 <sota.yamashita@gmail.com>
2014-04-10 10:46:07 +0900

    Remove: exporter_spec.rb: extra code

--- spec/cal_exporter/exporter_spec.rb
+++ spec/cal_exporter/exporter_spec.rb
@@ -1,14 +1,22 @@
 require 'spec_helper'
 
-describe Expoter do
+describe Exporter do
 
   before do
     @format = "jekyll"
     @save_location = "~/Desktop/meetups"
   end
 
-  it "should create yml file" do
-  	Expoter.new()
+  it "should create file for jeykyll" do
+  	
+  end
+
+  it "should create file specified its name" do
+
+  end
+
+  it "should return url array" do 
+  	
   end
    
 
