2d744544d98c544e7f05c4cb276b0dd000235cc8
sota0805 <sota.yamashita@gmail.com>
2014-04-10 14:10:45 +0900

    Fix: spec_helper.rb: status badges

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'cal_exporter'
 require 'cal_exporter/exporter'
 require 'cal_exporter/importer'
+require 'coveralls'
 Coveralls.wear!
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
