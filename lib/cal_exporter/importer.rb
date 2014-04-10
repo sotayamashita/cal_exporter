7e6b8c099e21a8142df6240f988bb524b2b3bcea
sota0805 <sota.yamashita@gmail.com>
2014-04-10 13:41:28 +0900

    Fix: importer.rb: module

--- lib/cal_exporter/importer.rb
+++ lib/cal_exporter/importer.rb
@@ -1,7 +1,6 @@
 module CalExporter
   class Importer 
 
-
     def initialize(calendar_id)
       @calendar_id = calendar_id
     end
@@ -11,7 +10,6 @@ module CalExporter
       calender_url = "https://www.google.com/calendar/ical/#{@calendar_id}/public/basic.ics"  
       calendars = open (calender_url) { |cal| Icalendar.parse(cal) }
     end
-
 
   end
 end
