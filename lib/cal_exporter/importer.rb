b2c17f8cfd4a766f4b17f6289a2a643e02790d94
sota0805 <sota.yamashita@gmail.com>
2014-04-10 11:45:12 +0900

    Add: importer.rb: module

--- lib/cal_exporter/importer.rb
+++ lib/cal_exporter/importer.rb
@@ -1,13 +1,17 @@
-class Importer 
+module CalExporter
+  class Importer 
 
-  def initialize(calendar_id)
-    @calendar_id = calendar_id
-  end
+
+    def initialize(calendar_id)
+      @calendar_id = calendar_id
+    end
   
 
-  def fetch
-    calender_url = "https://www.google.com/calendar/ical/#{@calendar_id}/public/basic.ics"  
-    calendars = open (calender_url) { |cal| Icalendar.parse(cal) }
-  end
+    def fetch
+      calender_url = "https://www.google.com/calendar/ical/#{@calendar_id}/public/basic.ics"  
+      calendars = open (calender_url) { |cal| Icalendar.parse(cal) }
+    end
 
+
+  end
 end
