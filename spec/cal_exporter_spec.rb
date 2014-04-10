a974c9763af2bfa4811195cd0ac8d195253c2821
sota0805 <sota.yamashita@gmail.com>
2014-04-10 10:44:25 +0900

    Remove: cal_exporter_spec.rb: pending message

--- spec/cal_exporter_spec.rb
+++ spec/cal_exporter_spec.rb
@@ -1,14 +1,12 @@
 require 'spec_helper'
 
 describe CalExporter do
-  it "is a pending"
 
   it 'should have a version number' do
      expect(CalExporter::VERSION).not_to be nil
   end
 
-  # subject (Object) {CalExporter.new} 
-  # it 
+  
 end
 
 
